a25b7234d0f0e40e35fc21cca3a6b288ac63dd2c
Cloud <cloudcry@gmail.com>
2020-12-07 12:59:03 +0800

    update: new solution for day 1 puzzle

--- day1/year_math.hs
+++ day1/year_math.hs
@@ -8,6 +8,7 @@
 import Data.List
 import Data.Bits
 import qualified Data.Text as T
+import Data.Maybe
 
 type Entry = Int
 type Answer = (Int, Int)
@@ -21,19 +22,10 @@ parseRaw :: String -> Entry
 parseRaw = read
 
 results :: [Entry] -> Answer
-results yrs = (p1, p2)
+results yrs = (pResult pairs, pResult triple)
   where
-    -- p1 = head [y1*y2 | y1 <- yrs, y2 <- yrs, y1 + y2 == 2020]
-    --p1 = head [y1*(2020 - y1) | y1 <- sortedYrs, 2020 - y1 `elem` sortedYrs]
-    p1 = product $ searchPairInt 2020 sortedYrs
-    --p2 = head [y1*y2*y3 | y1 <- yrs, y2 <- yrs, y3 <- yrs, y1 + y2 + y3 == 2020]
-    --p2 = head [y1*y2*y3 | y1 <- sortedYrs, y2 <- sortedYrs, y3 <- sortedYrs, y1 + y2 + y3 == 2020]
-    p2 = product $ searchTripleInt sortedYrs
-    sortedYrs = sort yrs
-    searchPairInt _ [] = [0]
-    searchPairInt sumInt (x:xs) = if sumInt - x `elem` xs then [x, sumInt - x] else searchPairInt sumInt xs
-    searchTripleInt xs@(x:y:_) = let subInts = filter (<= 2020 - (x + y)) xs in searchPairFor subInts (map (2020 -) subInts)
-                                     where 
-                                       searchPairFor _ [] = [0]
-                                       searchPairFor subInts (x:xs) = let pair = searchPairInt x subInts in if length pair  >= 2 then (2020-x):pair else searchPairFor subInts xs
-
+    pResult f = fromMaybe 0 . fmap product . find ((== 2020) . sum) . f $ sort yrs
+    pairs [] = []
+    pairs (x:xs) = map (\e -> [x, e]) xs ++ pairs xs
+    triple [] = []
+    triple (x:y:xs) = map (\e -> [x,y,e]) xs ++ triple (y:xs)
