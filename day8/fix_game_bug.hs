f4c26fb64a4f3b47dd7d512a64f686246b164b99
Cloud <cloudcry@gmail.com>
2020-12-12 10:03:13 +0800

    update: add graph solution and fix bug

--- day8/fix_game_bug.hs
+++ day8/fix_game_bug.hs
@@ -12,11 +12,17 @@ import Data.Maybe
 import Control.Monad
 import Data.Bifunctor
 import Data.Tuple
+import Data.Graph
+import Data.Tree
+import qualified Data.Sequence as S
+import Data.Foldable
 
 data Instruction = Nop Int|Acc Int|Jmp Int deriving Show
 
 type Entry = Instruction
 type Answer = (Int, Int)
+
+testInputs = ["nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6"]
 
 main = interact $ showResult . results . map parseRaw . lines
 
@@ -33,7 +39,7 @@ parseRaw s = case words s of
     pre x = fromMaybe x . stripPrefix "+" $ x
 
 results :: [Entry] -> Answer
-results insSet = swap . bimap (fst . head) (fst . head) . mapAccumL f [] . map stepsFrom $ allInsSetList
+results insSet = swap . bimap (fst . head) (fst . head) . mapAccumL f [] . map (stepsFrom (length insSet - 1)) $ allInsSetList
   where
     f a steps = let firstDupIndex = findDupIndex steps 
                  in if firstDupIndex == Nothing 
@@ -51,18 +57,6 @@ results insSet = swap . bimap (fst . head) (fst . head) . mapAccumL f [] . map s
                                          Nop _ -> True
                                          _ -> False) $ zip [0..] insSet
 
-    p1 = let steps = stepsFrom insSet 
-          in fromMaybe 0 . fmap (fst . (steps !!) . ((+) (-1))) $ findDupIndex steps
-  
-stepsFrom insSet = steps 0 0
-  where
-    steps value pos
-      | pos >= length insSet = []
-      | otherwise = (value, pos) : case insSet !! pos of 
-                                       Jmp n -> steps value (pos + n)
-                                       Acc n -> steps (value + n) (pos + 1)
-                                       Nop _ -> steps value (pos + 1)
-
 findDupIndex :: [(Int, Int)] -> Maybe Int
 findDupIndex [] = Nothing
 findDupIndex (x:[]) = Nothing
@@ -72,4 +66,46 @@ findDupIndex es = search [] es
     search xs (y:ys) = let index = findIndex (\e -> snd e == snd y) xs 
                         in if index == Nothing 
                               then search (y:xs) ys 
-                              else Just $ (length xs) + 1
+                              else Just $ (length xs)
+
+results2 :: [Entry] -> Answer
+results2 insSet = (fst . last . stepsFrom cyclicPos $ insSet, fst . last . stepsFrom (insSetLength - 1) . switchInsAt changablePos $ insSet)
+  where
+    cyclicPos = last startReachableList
+    changablePos = fst . head $ findConnectNodes startReachableList endReachableList newOps
+    insSetLength = length insSet
+    insGraph = buildG (0, insSetLength). insEdgesSet  $ zip [0..] insSet
+    startReachableList = reachable insGraph 0
+    endReachableList = reachable (transposeG insGraph) (insSetLength - 1)
+    newOps = insEdgesSet $ allChangableOps insSet
+
+stepsFrom stop insSet = steps 0 (0, 0)
+  where
+    steps value p
+      | stop == fst p = []
+      | otherwise = let pos = snd p in case insSet !! pos of 
+                      Jmp n -> (value, pos) : steps value (pos, pos + n)
+                      Acc n -> (value + n, pos) : steps (value + n) (pos, pos + 1)
+                      Nop _ -> (value, pos) : steps value (pos, pos + 1)
+
+
+allChangableOps :: [Instruction] -> [(Int, Instruction)]
+allChangableOps = foldr (\(index, is) b -> case is of 
+                            Nop n -> if n > 0 then (index, Jmp n):b else b 
+                            Jmp n -> (index, Nop n):b
+                            _ -> b) [] . zip [0..]
+
+findConnectNodes startSet endSet = filter (\(u, v) -> u `elem` startSet && v `elem` endSet)
+
+switchInsAt index insSet = updateList index  (case (insSet !! index) of
+                                            Jmp n -> Nop n
+                                            Nop n -> Jmp n) insSet
+
+updateList :: Int -> a -> [a] -> [a]
+updateList index v = toList . S.update index v . S.fromList
+
+insEdgesSet :: [(Int, Instruction)] -> [(Int, Int)]
+insEdgesSet [] = []
+insEdgesSet ((index, is):insSet) = (case is of 
+                                     Jmp m -> (index, index + m )
+                                     _ -> (index, index + 1)) : insEdgesSet insSet
